f7e184e111e2ab4c704000d6a4a3cdb94a4f7aa6
SohailSayed <mr.sohailsayed@gmail.com>
2025-05-05 03:25:28 -0400

    finish sidebar styling

--- app/sidebar/Profile.tsx
+++ app/sidebar/Profile.tsx
@@ -14,9 +14,14 @@ const Profile = ({}) => {
             </div>
             <p className={`${sonomaMed.className}`} style={{ fontSize: '30px', margin: '0', paddingTop: '20px'}}>Sohail</p>
             <p className={`${sonomaReg.className}`} style={{ fontSize: '15px', marginTop: '0' }}>Goated Intern 🐐</p>
+            <div style={{display: "flex"}}>
+                <img src={'/telephone.png'} style={{ height: '20px', marginRight: '30px', opacity: 0.6}}/>
+                <img src={'/speech-bubble.png'} style={{ height: '20px', marginLeft: '30px', opacity: 0.6}}/>
+            </div>  
         </div>
         <div className={styles.propertAccess}> 
-            <p className={`${sonomaReg.className}`} style={{ fontSize: '15px', marginTop: '0' }}>Edit Property Access</p>
+            <img src={'/edit-text.png'} style={{ height: '15px', marginRight: '5px', opacity: 0.8}}/>
+            <p className={`${sonomaReg.className}`} style={{ fontSize: '15px', marginTop: '0'}}>Edit Property Access</p>
         </div>
     </div>
   );
